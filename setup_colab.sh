6277e37ef94a36e2505e9580ab72b9263d7fad47
Mohamad AlMdfaa <mohamad.almdfaa@gmail.com>
2022-08-22 06:04:05 -0700

    chore(deps): add setup colab

---
+++ setup_colab.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+apt-get -qq update
+apt-get -qq install -y xvfb
+wget -q https://raw.githubusercontent.com/mhd-medfa/IU-Reinforcement-Learning-22-lab/main/xvfb -O ../xvfb
+
+# Download & import Atari ROMs (Colab stopped bundling them around the beginning of June 2021)
+
+gdown -q https://drive.google.com/uc?id=1wYEhsBbh3hc2tek9-Afj9BHSa7_WJkV8
+
+# Alternative download:
+# wget -q http://www.atarimania.com/roms/Roms.rar
+
+pip install -q unrar
+mkdir ./roms
+unrar x Roms.rar ./roms > /dev/null 2>&1
+python -m atari_py.import_roms ./roms > /dev/null 2>&1
